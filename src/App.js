edaeda7c5a86d337aba0661e662d76b7ddb7860f
jcxiao <chang.x1994@gmail.com>
2021-03-24 18:22:26 -0400

    Fixed CSS issue with built app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Page404 from './components/Page404';
 import Navbar from './components/Navbar';
 import SideNav from './components/Sidenav';
