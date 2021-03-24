98acf7e49c04043742444c1530458e3684a97a79
jcxiao <chang.x1994@gmail.com>
2021-03-24 15:38:25 -0400

    Added new projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,9 +3,13 @@ import jobsearchImg from "../../media/jobsearch.png";
 import quizgameImg from "../../media/quizgame.png";
 import schedulerImg from "../../media/scheduler.png";
 import weatherImg from "../../media/weather.png";
+import fluxImg from "../../media/fluxim.png";
+import budgetImg from "../../media/budget.png";
 
 const projects = [
+  { name:"Flux IM", image:fluxImg, link:"https://flux-im.herokuapp.com/", gitLink:"https://github.com/Dragontalker/Project2_fluxIM" },
   { name:"Job Search Aggregator", image:jobsearchImg, link:"https://teamcirclealpha.github.io/GetMeEmployed/", gitLink:"https://github.com/TeamCircleAlpha/GetMeEmployed" },
+  { name:"Budget Tracker", image:budgetImg, link:"https://jcx-budgettracker.herokuapp.com/", gitLink:"https://github.com/sinsinkun/UTOR-BudgetTracker" },
   { name:"Quiz Game", image:quizgameImg, link:"https://sinsinkun.github.io/UTOR-QuizGame/", gitLink:"https://github.com/sinsinkun/UTOR-QuizGame" },
   { name:"Scheduler", image:schedulerImg, link:"https://sinsinkun.github.io/UTOR-Scheduler/", gitLink:"https://github.com/sinsinkun/UTOR-Scheduler" },
   { name:"Weather dashboard", image:weatherImg, link:"https://sinsinkun.github.io/UTOR-WeatherApp/", gitLink:"https://github.com/sinsinkun/UTOR-WeatherApp" }
